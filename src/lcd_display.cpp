f1152fd7b6f0e84fce21e7422504d6619ca83992
mathis.autant <mathisautant@protonmail.com>
2023-11-17 23:37:59 +0100

    FEAT: temp on line1, humidity on line2

--- src/lcd_display.cpp
+++ src/lcd_display.cpp
@@ -5,16 +5,17 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
+  // Baud rate is set to 9600
+  Serial.begin(9600); 
   // Set up the LCD's number of columns and rows
   lcd.begin(16, 2);
 
   // Print temperature on the first line
   lcd.setCursor(0, 0);
-  lcd.print("coucou");
-
+  lcd.print("Temp.(°C): 21");
   // Print humidity on the second line
-  // lcd.setCursor(0, 1);
-  // lcd.print("Rel. humid.: 81%");
+  lcd.setCursor(0, 1);
+  lcd.print("Rel. humid.: 81%");
 }
 
 void loop() {
